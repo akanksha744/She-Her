dad36530debf5478ef1eb92604b8c34da078917e
akanksha744 <136923367+akanksha744@users.noreply.github.com>
2023-10-01 03:14:34 +0530

    Update resp.js

--- resp.js
+++ resp.js
@@ -10,3 +10,38 @@ burger.addEventListener('click',()=>{
     navbar.classList.toggle('h-nav-resp');
 
 })
+
+
+var nav = document.querySelector('.navbar');
+var homeSection = document.querySelector('#home');
+
+function updateNavbarBackground() {
+    if (window.scrollY + 300 > homeSection.offsetHeight) {
+        nav.classList.add('navbar-bg');
+    } else {
+        nav.classList.remove('navbar-bg');
+    }
+}
+
+updateNavbarBackground();
+window.addEventListener('scroll', updateNavbarBackground);
+
+
+
+
+const navLinks = document.querySelectorAll('header .navbar .nav-list a');
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  navLinks.forEach(link => {
+    const sectionId = link.getAttribute('href').substring(1);
+    const section = document.getElementById(sectionId);
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= 50 && rect.bottom >= 50) {
+      link.style.color = 'pink';
+    } else {
+      link.style.color = 'white'; 
+    }
+  });
+});
